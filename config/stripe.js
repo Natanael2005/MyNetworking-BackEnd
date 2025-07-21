0cdd63a64d6776e8dbe35faa8ed4050437097db3
Natanael2005 <165595367+Natanael2005@users.noreply.github.com>
2025-07-20 23:09:37 -0500

    validate stripe env vars

--- config/stripe.js
+++ config/stripe.js
@@ -2,7 +2,15 @@
 
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+// Validar que la clave secreta de Stripe esté configurada
+const { STRIPE_SECRET_KEY } = process.env;
+if (!STRIPE_SECRET_KEY) {
+  const msg = 'Falta STRIPE_SECRET_KEY en las variables de entorno';
+  console.error(msg);
+  throw new Error(msg);
+}
+
+const stripe = new Stripe(STRIPE_SECRET_KEY, {
   apiVersion: '2022-11-15'
 });
 export default stripe;
